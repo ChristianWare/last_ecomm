59af3f706f16215b03025c29e8e3426c048487c2
Chris Ware <c.p.ware@hotmail.com>
2023-09-21 16:15:49 -0700

    Started Signup page + added Formik Validation

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React from "react";
+import AuthFormContainer from "@/app/components/AuthFormContainer";
+import { Button, Input } from "@material-tailwind/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  nmae: yup.string().required("Name is required!"),
+  email: yup.string().email("Invalid Email!").required("Email is required!"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters!")
+    .required("Password is required!"),
+});
+
+export default function SignUp() {
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    errors,
+    touched,
+  } = useFormik({
+    initialValues: { name: "", email: "", password: "" },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const formErrors: string[] = [];
+
+  console.log(errors);
+
+  const { email, name, password } = values;
+
+  return (
+    <AuthFormContainer title='Create New Account' onSubmit={handleSubmit}>
+      <Input
+        name='name'
+        label='Name'
+        crossOrigin={undefined}
+        onChange={handleChange}
+        value={name}
+      />
+      <Input
+        name='email'
+        label='Email'
+        crossOrigin={undefined}
+        onChange={handleChange}
+        value={email}
+      />
+      <Input
+        name='password'
+        label='Password'
+        type='password'
+        crossOrigin={undefined}
+        onChange={handleChange}
+        value={password}
+      />
+      <Button type='submit' className='w-full'>
+        Sign up
+      </Button>
+      <div className=''>
+        {formErrors.map((err) => {
+          return (
+            <div key={err} className='space-x-1 flex items-center text-red-500'>
+              <XMarkIcon className='w-4 h-4' />
+              <p className='text-xs'>{err}</p>
+            </div>
+          );
+        })}
+      </div>
+    </AuthFormContainer>
+  );
+}
